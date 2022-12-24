e5d3c254f81d58c081d8fbe1c1584022649dd700
fateme zahra <fatemezahrakalantarii@gmail.com>
2022-12-24 19:12:48 +0330

    add Base.js

---
+++ model/Base.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes, name, model, config) => {
+  // config
+  if (config == undefined) config = {};
+  if (config.freezeTableName == undefined) config.freezeTableName = true;
+  if (config.tableName == undefined) config.tableName = name;
+  if (config.underscored == undefined) config.underscored = true;
+  config.timestamps = true;
+  config.paranoid = false;
+  config.createdAt = true;
+  config.updatedAt = true;
+  return sequelize.define(name, model, config);
+};
