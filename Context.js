9c1c57dfbba750624e011c16a7a599fea0f04df6
fateme zahra <fatemezahrakalantarii@gmail.com>
2022-12-29 23:52:21 +0330

    Set estate & estate_type

--- Context.js
+++ Context.js
@@ -9,14 +9,24 @@ module.exports = class Context {
     // Models
     const User = require("./model/User");
     const UserType = require("./model/UserType");
+    const Estate = require("./model/Estate");
+    const EstateType = require("./model/EstateType");
 
     // Tables
     const user = User(this.database, Sequelize.DataTypes);
     const user_type = UserType(this.database, Sequelize.DataTypes);
+    const estate = Estate(this.database, Sequelize.DataTypes);
+    const estate_type = EstateType(this.database, Sequelize.DataTypes);
 
     // ForeignKeys
     user.belongsTo(user_type, {
       foreignKey: { name: "user_type_id", allowNull: false },
+    });
+    estate.belongsTo(user, {
+      foreignKey: { name: "owner_id", allowNull: false },
+    });
+    estate.belongsTo(estate_type, {
+      foreignKey: { name: "estate_type_id", allowNull: false },
     });
 
     this.database.sync({ force: false });
