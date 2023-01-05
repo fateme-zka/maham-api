6428197ea0e67bf4d20f59b4158e8af5d755c00d
fateme zahra <fatemezahrakalantarii@gmail.com>
2023-01-06 00:56:26 +0330

    User Role position

--- model/Role.js
+++ model/Role.js
@@ -3,6 +3,7 @@ const Base = require("./Base");
 module.exports = (sequelize, DataTypes) => {
   return Base(sequelize, DataTypes, "role", {
     id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true, },
+    position: { type: DataTypes.TINYINT, allowNull: false },
     name: { type: DataTypes.STRING, allowNull: false },
     permissions: { type: DataTypes.JSON, allowNull: false },
   });
