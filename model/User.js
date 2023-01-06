4b44226eefba51051a45069bdbf0497ab0bba5b9
fateme zahra <fatemezahrakalantarii@gmail.com>
2023-01-06 14:43:42 +0330

    username unique

--- model/User.js
+++ model/User.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
     id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true, },
     role_id: { type: DataTypes.INTEGER, allowNull: false },
     admin: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
-    username: { type: DataTypes.STRING, allowNull: false },
+    username: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false },
     first_name: { type: DataTypes.STRING, allowNull: false },
     last_name: { type: DataTypes.STRING, allowNull: true },
