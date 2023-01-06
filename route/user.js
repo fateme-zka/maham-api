6d7e25f4229e54658036b0e223199e6aa425aa17
fateme zahra <fatemezahrakalantarii@gmail.com>
2023-01-06 23:38:14 +0330

    rename register endpionts

--- route/user.js
+++ route/user.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const requestHandler = require("../middleware/requestHandler");
 
-const registerCustomerController = require("../controller/user/register_customer");
+const registerCustomerController = require("../controller/user/register");
+const adminRegisterController = require("../controller/user/admin_register");
 
-router.post("/register_customer", requestHandler(registerCustomerController));
+router.post("/register", requestHandler(registerCustomerController));
+router.post("/admin_register", requestHandler(adminRegisterController));
 
 module.exports = router;
