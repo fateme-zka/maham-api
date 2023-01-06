c4ec38c755873b740a4b069e7a5cb72514aa805d
fateme zahra <fatemezahrakalantarii@gmail.com>
2023-01-06 12:57:25 +0330

    route file

--- index.js
+++ index.js
@@ -18,17 +18,17 @@ const context = new Context();
 context.init();
 
 // Api Routes
-app.use("/estate", require("./route/estate"));
+app.use("/", require("./route/route"));
 
 // TESTING DATABASE CONNECTION:
-const db = require("./config/database");
-db.authenticate()
-  .then(() => {
-    Winston.info("DB Connection has been established successfully.");
-  })
-  .catch((error) => {
-    Winston.error(error);
-  });
+// const db = require("./config/database");
+// db.authenticate()
+//   .then(() => {
+//     Winston.info("DB Connection has been established successfully.");
+//   })
+//   .catch((error) => {
+//     Winston.error(error);
+//   });
 
 // Start server
 const port = process.env.server_port;
