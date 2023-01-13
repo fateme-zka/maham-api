76b603f17612f79c2c990d387adb041a7e60b7f6
fateme zahra <fatemezahrakalantarii@gmail.com>
2023-01-14 00:12:46 +0330

    add estate

--- route/estate.js
+++ route/estate.js
@@ -3,7 +3,13 @@ const router = require("express").Router();
 const requestHandler = require("../middleware/requestHandler");
 
 const getAllController = require("../controller/estate/getAll");
+const postController = require("../controller/estate/add");
 
-router.get("/all", requestHandler(getAllController));
+router.get("/all", requestHandler(getAllController)); // should get query
+router.post("/:estate_type/:sale_method/add", requestHandler(postController));
+// router.put("/update/:id", requestHandler(updateController));
+// router.delete("/delete/:id", requestHandler(deleteController));
+// router.get("/switch/activation/:id", requestHandler(switchActivationController));
+// router.get("/verify/:id", requestHandler(verifyController))
 
 module.exports = router;
