21f3f7ea8c2aa71f63fd905cf0529b07accd2be6
fateme zahra <fatemezahrakalantarii@gmail.com>
2023-01-14 20:46:25 +0330

    edit EstateScore model

--- model/EstateScore.js
+++ model/EstateScore.js
@@ -5,9 +5,6 @@ module.exports = (sequelize, DataTypes) => {
   return Base(sequelize, DataTypes, "estate_score", {
     id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true, },
     estate_id: { type: DataTypes.INTEGER, allowNull: false },
-    score: {
-        type: Sequelize.ENUM(1, 2, 3, 4, 5),
-        allowNull: false,
-    },
+    score: { type: DataTypes.INTEGER, allowNull: false }, // 0 to 5
   });
 };
