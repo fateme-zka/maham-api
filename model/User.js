213b8f6aefc8960e1eef85cd2c4039aa8937a76b
fateme zahra <fatemezahrakalantarii@gmail.com>
2023-04-21 00:52:23 +0330

    edit User model

--- model/User.js
+++ model/User.js
@@ -1,26 +1,25 @@
 const Base = require("./Base");
 
-module.exports = (sequelize, DataTypes) => {
-  return Base(sequelize, DataTypes, "user", {
-    id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true, },
-    role_id: { type: DataTypes.INTEGER, allowNull: false },
-    admin: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
-    username: { type: DataTypes.STRING, allowNull: false, unique: true },
-    password: { type: DataTypes.STRING, allowNull: false, secure: true  },
-    first_name: { type: DataTypes.STRING, allowNull: false },
-    last_name: { type: DataTypes.STRING, allowNull: true },
-    phone_number: { type: DataTypes.STRING, allowNull: false, unique: true },
-    email: { type: DataTypes.STRING, allowNull: true },
-    image: { type: DataTypes.STRING, allowNull: true },
-    cover_image: { type: DataTypes.STRING, allowNull: true },
-    e_otp: { type: DataTypes.STRING(6), allowNull: true, secure: true },
-    e_otp_time: { type: 'TIMESTAMP', allowNull: true, defaultValue: sequelize.literal('CURRENT_TIMESTAMP'), secure: true },
-    e_otp_attempt: { type: DataTypes.INTEGER, defaultValue: 0, allowNull: false, secure: true },
-    e_try_attempt: { type: DataTypes.INTEGER, defaultValue: 0, allowNull: false, secure: true },
-    e_approved: { type: DataTypes.BOOLEAN, defaultValue: false, allowNull: false },
-    f_otp: { type: DataTypes.STRING(18), allowNull: true, secure: true },
-    f_otp_time: { type: 'TIMESTAMP', allowNull: true, defaultValue: sequelize.literal('CURRENT_TIMESTAMP'), secure: true },
-    f_otp_attempt: { type: DataTypes.INTEGER, defaultValue: 0, allowNull: false, secure: true },
-    f_try_attempt: { type: DataTypes.INTEGER, defaultValue: 0, allowNull: false, secure: true },
-  });
+module.exports = (sequelize, DataTypes) =>
+{
+	return Base(sequelize, DataTypes, "user", {
+		id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true, },
+		role_id: { type: DataTypes.INTEGER, allowNull: false },
+		admin: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
+		password: { type: DataTypes.STRING, allowNull: false, secure: true },
+		name: { type: DataTypes.STRING, allowNull: false },
+		phone_number: { type: DataTypes.STRING, allowNull: false, unique: true },
+		email: { type: DataTypes.STRING, allowNull: true, unique: true },
+		image: { type: DataTypes.STRING, allowNull: true },
+		cover_image: { type: DataTypes.STRING, allowNull: true },
+		e_otp: { type: DataTypes.STRING(6), allowNull: true, secure: true },
+		e_otp_time: { type: 'TIMESTAMP', allowNull: true, defaultValue: sequelize.literal('CURRENT_TIMESTAMP'), secure: true },
+		e_otp_attempt: { type: DataTypes.INTEGER, defaultValue: 0, allowNull: false, secure: true },
+		e_try_attempt: { type: DataTypes.INTEGER, defaultValue: 0, allowNull: false, secure: true },
+		e_approved: { type: DataTypes.BOOLEAN, defaultValue: false, allowNull: false },
+		f_otp: { type: DataTypes.STRING(18), allowNull: true, secure: true },
+		f_otp_time: { type: 'TIMESTAMP', allowNull: true, defaultValue: sequelize.literal('CURRENT_TIMESTAMP'), secure: true },
+		f_otp_attempt: { type: DataTypes.INTEGER, defaultValue: 0, allowNull: false, secure: true },
+		f_try_attempt: { type: DataTypes.INTEGER, defaultValue: 0, allowNull: false, secure: true },
+	});
 };
