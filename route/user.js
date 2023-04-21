3723097364465b9f001800921eb069d1ab1c9d3d
fateme zahra <fatemezahrakalantarii@gmail.com>
2023-04-21 13:01:20 +0330

    remove logout api

--- route/user.js
+++ route/user.js
@@ -6,14 +6,12 @@ const getController = require("../controller/user/get");
 const listController = require("../controller/user/getUsers");
 const registerController = require("../controller/user/register");
 const loginController = require("../controller/user/login");
-const logoutController = require("../controller/user/logout");
 const updateController = require("../controller/user/update");
 
 router.get("/:id", requestHandler(getController));
 router.get("/list", requestHandler(listController));
 router.post("/register", requestHandler(registerController));
 router.post("/login", requestHandler(loginController));
-router.post("/logout", requestHandler(logoutController));
 router.put("/update/:id", requestHandler(updateController));
 
 module.exports = router;
