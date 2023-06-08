35173cdcb6de24561144660a834dbad7c3ed4b8e
fateme zahra <fatemezahrakalantarii@gmail.com>
2023-06-08 23:43:00 +0330

    get estates and customers list in estate/followup API

--- route/estate/followup.js
+++ route/estate/followup.js
@@ -3,12 +3,16 @@ const router = require("express").Router();
 const requestHandler = require("../../middleware/requestHandler");
 
 
+const getAllEstatesController = require("../../controller/estate/followup/getAllEstates");
+const getAllCustomersController = require("../../controller/estate/followup/getAllCustomers");
 // const getAllFollowupsController = require("../../controller/estate/followup/getAllEstateFollowup");
 // const postFollowupController = require("../../controller/estate/followup/postEstateFollowup");
 // const putFollowupController = require("../../controller/estate/followup/putEstateFollowup");
 // const deleteFollowupController = require("../../controller/estate/followup/deleteEstateFollowup");
 
 
+router.get("/estate/all", requestHandler(getAllEstatesController));
+router.get("/customer/all", requestHandler(getAllCustomersController));
 // router.get("/all", requestHandler(getAllFollowupsController));
 // router.post("/", requestHandler(postFollowupController));
 // router.put("/:id", requestHandler(putFollowupController));
