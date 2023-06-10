3970c0d61f3ed28277bbde7d0361cab04275d3d3
fateme zahra <fatemezahrakalantarii@gmail.com>
2023-06-10 23:19:51 +0330

    upload or remove setting logo APIs

--- controller/admin/setting/addLogo.js
+++ controller/admin/setting/addLogo.js
@@ -6,12 +6,11 @@ const body_schema = Joi.object({});
 const handler = async (req, res) =>
 {
 	let { image } = req.files;
-	let { table } = req.params;
 
 	let md5 = image.md5;
 	let ext = Path.extname(image.name);
-	let path = `/static/upload/${table}/image/${md5}${ext}`;
-	image.mv(__dirname + '/../../static' + path);
+	let path = `/static/upload/setting/logo${ext}`;
+	image.mv(__dirname + '/../../../static' + path);
 
 	return {
 		url: process.env.BACKEND_BASE_URL + path
--- route/admin/setting.js
+++ route/admin/setting.js
@@ -3,13 +3,17 @@ const router = require("express").Router();
 const requestHandler = require("../../middleware/requestHandler");
 
 const getAllSettingsController = require("../../controller/admin/setting/getAll");
+const addLogoController = require("../../controller/admin/setting/addLogo");
 const putMetaTagsController = require("../../controller/admin/setting/putMetaTags");
 const putContactController = require("../../controller/admin/setting/putContact");
 const putSiteController = require("../../controller/admin/setting/putSite");
+const removeLogoController = require("../../controller/admin/setting/deleteLogo");
 
 router.get("/all", requestHandler(getAllSettingsController));
+router.post("/upload/logo", requestHandler(addLogoController));
 router.put("/meta/tag", requestHandler(putMetaTagsController));
 router.put("/contact", requestHandler(putContactController));
 router.put("/site", requestHandler(putSiteController));
+router.delete("/logo", requestHandler(removeLogoController));
 
 module.exports = router;
