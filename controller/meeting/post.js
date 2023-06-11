86a39539d0235be292517fb1d249b3905e4b1684
fateme zahra <fatemezahrakalantarii@gmail.com>
2023-06-12 00:05:04 +0330

    add new meeting API

---
+++ controller/meeting/post.js
@@ -0,0 +1,22 @@
+const Joi = require("joi");
+
+const body_schema = Joi.object({
+	estate_id: Joi.number().required(),
+	customer_id: Joi.number().required(),
+	title: Joi.string().required(),
+	address: Joi.string().required(),
+	description: Joi.string().required(),
+	time: Joi.string().regex(/^([0-9]{2})\:([0-9]{2})$/).required(),
+	date: Joi.date().required(),
+	send_sms: Joi.boolean().required(),
+});
+
+const handler = async function (req)
+{
+	let user_id = req.user.id;
+	let { estate_id, customer_id, title, address, description, time, date, send_sms } = req.body;
+
+	return await req.context.addMeeting(user_id, estate_id, customer_id, title, address, description, time, date, send_sms);
+};
+
+module.exports = { handler, body_schema, auth: true, auth_consultant: true };
