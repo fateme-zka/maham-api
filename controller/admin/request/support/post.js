81d9801a84793af885e7c9c8d7885882475949c2
fateme zahra <fatemezahrakalantarii@gmail.com>
2023-06-19 22:40:00 +0330

    send support request info

--- controller/admin/request/support/post.js
+++ controller/admin/request/support/post.js
@@ -1,5 +1,6 @@
 const Joi = require("joi");
 const Phone = require("phone");
+const email_operation = require("../../../../util/email_operation");
 
 const body_schema = Joi.object({
 	name: Joi.string().required(),
@@ -22,9 +23,38 @@ const handler = async function (req)
 	if (!Phone.phone(call_number).isValid)
 		req.throw(400, "Invalid call number."); // todo check 
 
-	// todo send request to brand site panel by axios
 
-	return await req.context.addSupportRequest(user.id, name, phone_number, call_number, title, description);
+	let support_request = await req.context.addSupportRequest(user.id, name, phone_number, call_number, title, description);
+
+	if (support_request)
+	{
+		let text = `
+		«Mahama amlak new Support request»
+		--------------------------------------
+		<Admin Info>
+		name: ${user.name}
+		email: ${user.email}
+
+		<Support Info>
+		name: ${support_request.name}
+		phone_number: ${support_request.phone_number}
+		call_number: ${support_request.call_number}
+		title: ${support_request.title}
+		description: ${support_request.description}
+		created_at: ${support_request.createdAt}
+		`;
+
+		email_operation.send(
+			process.env.requsest_receiver_email,
+			"Maham Amlak Support Request",
+			text
+		);
+
+		return {
+			message: "Support request sent",
+			support_request
+		}
+	}
 };
 
 module.exports = { handler, body_schema, auth: true, auth_admin: true };
