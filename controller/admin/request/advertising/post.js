c39b20d223c68d402897a468070847a178b4fd65
fateme zahra <fatemezahrakalantarii@gmail.com>
2023-06-19 22:37:02 +0330

    send advertising request email

--- controller/admin/request/advertising/post.js
+++ controller/admin/request/advertising/post.js
@@ -1,5 +1,6 @@
 const Joi = require("joi");
 const Phone = require("phone");
+const email_operation = require("../../../../util/email_operation");
 
 const body_schema = Joi.object({
 	name: Joi.string().required(),
@@ -35,9 +36,38 @@ const handler = async function (req)
 	if (!Phone.phone(call_number).isValid)
 		req.throw(400, "Invalid call number."); // todo check 
 
-	// todo send request to brand site panel by axios
+	let advertising_request = await req.context.addAdvertisingRequest(user.id, name, phone_number, call_number, ad_type, description);
 
-	return await req.context.addAdvertisingRequest(user.id, name, phone_number, call_number, ad_type, description);
+	if (advertising_request)
+	{
+		let text = `
+		«Mahama amlak new Advertisign request»
+		--------------------------------------
+		<Admin Info>
+		name: ${user.name}
+		email: ${user.email}
+
+		<Advertising Info>
+		name: ${advertising_request.name}
+		phone_number: ${advertising_request.phone_number}
+		call_number: ${advertising_request.call_number}
+		advertising type: ${advertising_request.ad_type}
+		description: ${advertising_request.description}
+		created_at: ${advertising_request.createdAt}
+		`;
+
+		email_operation.send(
+			process.env.requsest_receiver_email,
+			"Maham Amlak Advertising Request",
+			text
+		);
+
+		return {
+			message: "Advertising request sent",
+			advertising_request
+		}
+	}
+
 };
 
 module.exports = { handler, body_schema, auth: true, auth_admin: true };
